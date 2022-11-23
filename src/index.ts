69dcbcacf717073acb51e1272bc5491b68799d98
Brian Vestergaard Danielsen <bvd@ase.au.dk>
2022-11-23 15:24:18 +0100

    Add "Hello, World!" endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import * as functions from 'firebase-functions';
+import { HelloWorld } from './hello-world';
+
 setImmediate(() => {
   console.log('Hello, Node.js with TypeScript!')
 })
+
+exports.hello = functions.https.onRequest(HelloWorld);
