ed805827652a630fddc329cfd0b53e5450b467a4
Brian Vestergaard Danielsen <brian.v.danielsen@gmail.com>
2022-11-24 00:00:33 +0100

    Add trigger

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,12 +10,24 @@ admin.initializeApp();
 
 // Generated with `firebase init`
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
 
 // Hello from Express
 export const helloExpress = functions.https.onRequest(hello);
 
-// Users
+// User endpoint
 export const user = functions.https.onRequest(users);
+
+// Triggers
+export const userCreated = functions.firestore.document('user/{uid}').onCreate((snapshot, context) => {
+  const { eventId } = context;
+  const { name, email } = snapshot.data();
+  const ref = admin.firestore().collection('log').doc(eventId)
+  functions.logger.info('User created', { name: `${name.first} ${name.last}`, email})
+  return admin.firestore().doc(ref.path).set({ 
+    message: `${ name.first } ${name.last} created an account`, 
+    timestamp: admin.firestore.Timestamp.now()
+  });
+})
